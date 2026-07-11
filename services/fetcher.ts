d79f2078f69da4984949d17161b5875b2ba14365
MoyyyL <moyyyl0526@gmail.com>
2026-07-10 20:16:44 -0600

    production url

--- services/fetcher.ts
+++ services/fetcher.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://blog-api-5jjl.onrender.com";
 import { getOptions, postOptions /*apiEndpoints*/ } from "./fetchOptions";
 
 export async function post(params = {}, endpoint: string) {
